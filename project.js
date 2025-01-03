2d80d1cc458e02b5cd17d34362fb53c69d60eef2
D-ICON <delawadetunji@gmail.com>
2025-01-03 21:28:23 +0100

    four commit

--- project.js
+++ project.js
@@ -1,6 +1,9 @@
-for (let i = 0; i <= 100; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
+function reverseArray(arr) {
+  return arr.slice().reverse();
+}
+const numbers = [1,2,3,4,5];
+console.log(reverseArray(numbers));
+
+  
+  
   
